31cc23db8b6b8a6b3070c0504ce0a46e3bc5049d
dpayton23 <darien.payton71@gmail.com>
2019-02-25 18:19:33 -0500

    completed challenge 3

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -18,8 +18,8 @@ anotherFunction("Example");
 //   return param1 + param2;
 // };
 
-// let add = (param1, param2) => param1 + param2
-// add(1,2);/
+let add = (param1, param2) => param1 + param2
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
